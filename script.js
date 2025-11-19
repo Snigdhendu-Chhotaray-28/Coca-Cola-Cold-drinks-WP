8a2dd6c8dedcedf3f993a1d7299d63c39dd06e04
Snigdhendu-Chhotaray-28 <snigdhenduchhotaray@gmail.com>
2025-11-19 16:06:33 +0530

    initializeSwipersAutoPlay function

--- script.js
+++ script.js
@@ -22,6 +22,61 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    // === 2. Initialize Limited Edition Swiper with Autoplay Control: By Snigdhendu ===
+    function initializeSwipersAutoPlay() {
+        const leSwiper = new Swiper(".le_divs", {
+            loop: true,
+            slidesPerView: 3,
+            spaceBetween: 30,
+            autoplay: {
+                delay: 1000,
+                disableOnInteraction: false,
+                pauseOnMouseEnter: true,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            mousewheel: false,
+            freeMode: true,
+            speed: 2000,
+            breakpoints: {
+                0: {
+                    slidesPerView: 1
+                },
+                550: {
+                    slidesPerView: 2
+                },
+                800: {
+                    slidesPerView: 3
+                }
+            }
+        });
+
+        const leContainer = document.querySelector('.le_divs');
+        let width = leContainer.offsetWidth;
+
+        if(width < 703){
+            leSwiper.slidesPerView = 2;
+        }
+
+        if (leContainer) {
+            const observer = new IntersectionObserver((entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        leSwiper.autoplay.start();
+                    } else {
+                        leSwiper.autoplay.stop();
+                    }
+                });
+            }, {
+                threshold: 0.01
+            });
+
+            observer.observe(leContainer);
+        }
+    }
+
 
 });
 
